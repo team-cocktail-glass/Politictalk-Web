ec9feb732a7218bdcc0a9490106fa5c8c0b55dc7
SayBGM <bgm537@saybgm.me>
2018-09-15 11:38:40 +0900

    Main css 수정

--- src/container/main/index.js
+++ src/container/main/index.js
@@ -21,15 +21,17 @@ export default class Main extends React.Component{
         </ContantsImg>
         <Contants>
           <div className="Main">
-            <span className="Main__service">
-              주요 서비스
-            </span>
-            <div className="Main__ExplainMenu">
-              <span className={this.state.explainTab === 1 ? "Main__ExplainMenu__Menu --select" : "Main__ExplainMenu__Menu"} onClick={()=> this.ChangeExplain(1)}>불만 리포트</span>
-              <span className={this.state.explainTab === 2 ? "Main__ExplainMenu__Menu --select" : "Main__ExplainMenu__Menu"} onClick={()=> this.ChangeExplain(2)}>정치인 미팅</span>
-              <span className={this.state.explainTab === 3 ? "Main__ExplainMenu__Menu --select  --last" : "Main__ExplainMenu__Menu --last"} onClick={()=> this.ChangeExplain(3)}>법안 제안</span>
+            <div style={{margin:'0 auto'}}>
+              <span className="Main__service">
+                주요 서비스
+              </span>
+              <div className="Main__ExplainMenu">
+                <span className={this.state.explainTab === 1 ? "Main__ExplainMenu__Menu --select" : "Main__ExplainMenu__Menu"} onClick={()=> this.ChangeExplain(1)}>불만 리포트</span>
+                <span className={this.state.explainTab === 2 ? "Main__ExplainMenu__Menu --select" : "Main__ExplainMenu__Menu"} onClick={()=> this.ChangeExplain(2)}>정치인 미팅</span>
+                <span className={this.state.explainTab === 3 ? "Main__ExplainMenu__Menu --select  --last" : "Main__ExplainMenu__Menu --last"} onClick={()=> this.ChangeExplain(3)}>법안 제안</span>
+              </div>
+              <Explain index={this.state.explainTab}/>
             </div>
-            <Explain index={this.state.explainTab}/>
           </div>
         </Contants>
       </div>
